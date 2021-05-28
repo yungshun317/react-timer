3ccb3d230da44755c5c49ee74b7764c4ef5de319
yungshun317 <seraph317@gmail.com>
2021-05-28 17:35:50 +0800

    [FEAT] Implement the `Timer` component by extending the `React.Component` class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import createReactClass from "create-react-class";
 import React from "react";
 import ReactDom from "react-dom";
 
+/* [1] `React.createClass`` has been deprecated so we have to use the create-react-class package
 var Timer = createReactClass({
 	getInitialState: function() {
 		return { counter: this.props.initialSeconds };
@@ -20,6 +21,30 @@ var Timer = createReactClass({
 		return <div>{this.state.counter}</div>;
 	}
 });
+*/
+
+// [2] `React.Component``
+class Timer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { counter: this.props.initialSeconds };
+	}
+	componentDidMount() {
+		let currentCounter;
+		this.timerId = setInterval(() => {
+			currentCounter = this.state.counter;
+			if (currentCounter === 1) {
+				clearInterval(this.timerId);
+			}
+			this.setState({ counter: currentCounter - 1 });
+		}, 1000);
+	}
+	render() {
+		return (
+			<div>{this.state.counter}</div>
+		);
+	}
+}
 
 ReactDom.render(
 	<Timer initialSeconds={36} />,
