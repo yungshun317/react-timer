46d4ef76dd2fbb999f02b0984c4a58c0fe3febd2
yungshun317 <seraph317@gmail.com>
2021-05-28 10:39:27 +0800

    [CHORE] Install and configure html-webpack-plugin and Babel to compile JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -14,6 +15,18 @@ module.exports = {
 		contentBase: path.join(__dirname, "dist"),
 		compress: true,
 		port: 9000,
-		open: "Google Chrome"
-	}
+		open: "google-chrome"
+	},
+	module: {
+		rule: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: ["babel-loader"]
+		}]
+	},
+	plugins: [
+	    new HtmlWebpackPlugin({
+	    	template: path.resolve(__dirname, "src", "index.html")
+	    })
+	]
 }
